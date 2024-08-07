3439ce0d54f8b5a31c578ad21354b0208380b619
jburba <jacob.burba@manchester.ac.uk>
2024-08-07 15:49:46 +0000

    Changed run script back to sbatch script

--- hera/azimuth/run_pspec_LPXLTK.sh
+++ hera/azimuth/run_pspec_LPXLTK.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+#SBATCH --job-name test_pspec
+#SBATCH --nodes 1
+#SBATCH --ntasks-per-node 15
+#SBATCH --time 24:00:00
+#SBATCH --mem 128GB
+#SBATCH --output ./slurm-out/%J.out
 
-#source ~/.bashrc
-#conda activate h1c-idr3
-which python
-
+source ~/.bashrc
+conda activate h1c-idr3
 cd /project/power-spectra/h1c-idr3/subset/
 
-echo "start: $(date)" 
+echo "start: $(date)"
 pspec_dir=/project/src/skaeor/hera/azimuth
 ${pspec_dir}/../pspec_pipe.py ${pspec_dir}/pspec_params_LPXLTK.yaml
 
