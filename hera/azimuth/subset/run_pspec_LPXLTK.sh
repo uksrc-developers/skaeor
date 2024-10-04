cc90f2afbd449057c0b3aff41a2e606cc11c54fa
jburba <jacob.burba@manchester.ac.uk>
2024-10-04 14:03:49 +0000

    Create directories for subset and full HERA analyses on azimuth

--- hera/azimuth/subset/run_pspec_LPXLTK.sh
+++ hera/azimuth/subset/run_pspec_LPXLTK.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name test_pspec
 #SBATCH --nodes 1
-#SBATCH --ntasks-per-node 15
+#SBATCH --ntasks-per-node 10
 #SBATCH --time 24:00:00
 #SBATCH --mem 128GB
 #SBATCH --output ./slurm-out/%J.out
@@ -11,7 +11,7 @@ conda activate h1c-idr3
 cd /project/power-spectra/h1c-idr3/subset/
 
 echo "start: $(date)"
-pspec_dir=/project/src/skaeor/hera/azimuth
-${pspec_dir}/../pspec_pipe.py ${pspec_dir}/pspec_params_LPXLTK.yaml
+pspec_dir=/project/src/skaeor/hera/azimuth/subset
+${pspec_dir}/../../pspec_pipe.py ${pspec_dir}/pspec_params_LPXLTK.yaml
 
 echo "end: $(date)"
