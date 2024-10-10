5461d9fcfb7bf793475e614af1c1b93a706539ea
jburba <jburba@users.noreply.github.com>
2024-10-10 15:14:31 +0100

    Use max walltime to be safe

--- hera/azimuth/container/run_pspec_LPXLTK.sh
+++ hera/azimuth/container/run_pspec_LPXLTK.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes 1
 #SBATCH --ntasks-per-node 54
 #SBATCH --cpus-per-task 1
-#SBATCH --time 24:00:00
+#SBATCH --time 59-00:00:00
 #SBATCH --mem 160GB
 #SBATCH --output ./slurm-out/%J.out
 
